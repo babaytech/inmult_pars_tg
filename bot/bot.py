2836fced61930009d5cca8947b1beb6a19ee3d34
babay <80759213+babaytech@users.noreply.github.com>
2022-05-30 22:41:18 +0500

    Add files via upload

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,11 @@
 #####################################################################
 import configparser
+from loguru import logger
 import sqlite3
 import parser.south_park, parser.rick_and_morty, parser.adventury_time, parser.love_death_and_robots
-import colorama
-import os
 from moviepy.editor import *
 import wget
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
 #####################################################################
 
 config = configparser.ConfigParser()
@@ -21,15 +17,22 @@ dp = Dispatcher(bot)
 conn = sqlite3.connect('serial.sqlite3')
 cursor = conn.cursor()
 
+logger.add("log.log", compression="zip", rotation="500 MB")
+
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["Южный Парк", "Рик и Морти", "Любовь Смерть и Роботы", "Время Приключений"]
     keyboard.add(*buttons)
+    username = message.from_user.username if message.from_user.username else None
+    logger.debug(f"{username} подключился к боту")
     await message.answer("Добро Пожаловать\nвыберите что хотите посмотреть", reply_markup=keyboard)
+
 
 @dp.message_handler(commands="update_db")
 async def cmd_test1(message: types.Message):
+    username = message.from_user.username if message.from_user.username else None
+    logger.debug(f"{username} обновляет базу данных")
     await message.answer("ожидайте")
     parser.love_death_and_robots.main_mult()
     await message.answer("Любовь Смерть и Роботы записан в базу данных")
@@ -44,38 +47,97 @@ async def cmd_test1(message: types.Message):
 @dp.message_handler(content_types=['text'])
 async def main_btn(message : types.Message):
     if message.text == 'Южный Парк':
+        username = message.from_user.username if message.from_user.username else None
+        logger.debug(f"{username} нажал кнопку Южный Парк")
         number = 0
         db = "SELECT * FROM south_park"
+        await message.answer("Ожидайте Отправки Видео")
+        logger.info("Запуск Скачивания Серий Южного Парка")
         cursor.execute(db)
         url_db = cursor.fetchall()
         for row in url_db:
             download_urls = row[2]
             wget.download(download_urls, f"{number}.mp4")
+            logger.info(f"Сжатие файла {number}.mp4")
             clip = VideoFileClip(f"{number}.mp4")
             resize = clip.resize(0.5)
             resize.write_videofile(f"{number}_resized.mp4")
-            with open(f"{number}_resized.mp4", 'rb') as video:
-                await message.answer_video(video)
+            await message.answer_video(open(f'{number}_resized.mp4', 'rb'))
             await message.answer(f"{number}.mp4 Загружен на сервер!")
             os.remove(f"{number}.mp4")
             os.remove(f"{number}_resized.mp4")
+            logger.info(f"файл {number}_resized.mp4 отправлен пользователю")
+            number += 1
+
+    if message.text == 'Рик и Морти':
+        username = message.from_user.username if message.from_user.username else None
+        logger.debug(f"{username} нажал кнопку Рик и Морти")
+        number = 0
+        db = "SELECT * FROM rick_and_morty"
+        await message.answer("Ожидайте Отправки Видео")
+        logger.info("Запуск Скачивания Серий Рик и Морти")
+        cursor.execute(db)
+        url_db = cursor.fetchall()
+        for row in url_db:
+            download_urls = row[2]
+            wget.download(download_urls, f"{number}.mp4")
+            logger.info(f"Сжатие файла {number}.mp4")
+            clip = VideoFileClip(f"{number}.mp4")
+            resize = clip.resize(0.5)
+            resize.write_videofile(f"{number}_resized.mp4")
+            await message.answer_video(open(f'{number}_resized.mp4', 'rb'))
+            await message.answer(f"{number}.mp4 Загружен на сервер!")
+            os.remove(f"{number}.mp4")
+            os.remove(f"{number}_resized.mp4")
+            logger.info(f"файл {number}_resized.mp4 отправлен пользователю")
+            number += 1
+
+    if message.text == 'Любовь Смерть и Роботы':
+        username = message.from_user.username if message.from_user.username else None
+        logger.debug(f"{username} нажал кнопку Любовь, Смерть и Роботы")
+        number = 0
+        db = "SELECT * FROM love_death_and_robots"
+        await message.answer("Ожидайте Отправки Видео")
+        logger.info("Запуск Скачивания Серий Любовь, Смерть и Роботы")
+        cursor.execute(db)
+        url_db = cursor.fetchall()
+        for row in url_db:
+            download_urls = row[2]
+            wget.download(download_urls, f"{number}.mp4")
+            logger.info(f"Сжатие файла {number}.mp4")
+            clip = VideoFileClip(f"{number}.mp4")
+            resize = clip.resize(0.5)
+            resize.write_videofile(f"{number}_resized.mp4")
+            await message.answer_video(open(f'{number}_resized.mp4', 'rb'))
+            await message.answer(f"{number}.mp4 Загружен на сервер!")
+            os.remove(f"{number}.mp4")
+            os.remove(f"{number}_resized.mp4")
+            logger.info(f"файл {number}_resized.mp4 отправлен пользователю")
             number += 1
 
 
-    if message.text == 'Рик и Морти':
-        rm_btn_1 = InlineKeyboardButton(f'1 Сезон', callback_data='button1')
-        rm_kb1 = InlineKeyboardMarkup().add(rm_btn_1)
-        await message.reply('Выберете Сезон:', reply_markup=rm_kb1)
-
-    if message.text == 'Любовь Смерть и Роботы':
-        ldr_btn_1 = InlineKeyboardButton(f'1 Сезон', callback_data='button1')
-        ldr_kb1 = InlineKeyboardMarkup().add(ldr_btn_1)
-        await message.reply('Выберете Сезон:', reply_markup=ldr_kb1)
-
     if message.text == 'Время Приключений':
-        ta_btn_1 = InlineKeyboardButton(f'1 Сезон', callback_data='button1')
-        ta_kb1 = InlineKeyboardMarkup().add(ta_btn_1)
-        await message.reply('Выберете Сезон:', reply_markup=ta_kb1)
+        username = message.from_user.username if message.from_user.username else None
+        logger.debug(f"{username} нажал кнопку Время Приключений")
+        number = 0
+        db = "SELECT * FROM adventure_time"
+        await message.answer("Ожидайте Отправки Видео")
+        logger.info("Запуск Скачивания Серий Времени Приключений")
+        cursor.execute(db)
+        url_db = cursor.fetchall()
+        for row in url_db:
+            download_urls = row[2]
+            wget.download(download_urls, f"{number}.mp4")
+            logger.info(f"Сжатие файла {number}.mp4")
+            clip = VideoFileClip(f"{number}.mp4")
+            resize = clip.resize(0.5)
+            resize.write_videofile(f"{number}_resized.mp4")
+            await message.answer_video(open(f'{number}_resized.mp4', 'rb'))
+            await message.answer(f"{number}.mp4 Загружен на сервер!")
+            os.remove(f"{number}.mp4")
+            os.remove(f"{number}_resized.mp4")
+            logger.info(f"файл {number}_resized.mp4 отправлен пользователю")
+            number += 1
 
 def start_bot():
     executor.start_polling(dp, skip_updates=True)
